44214e5bb8e9572e799f90bbecd441527b9c75e2
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-12-15 22:55:51 -0600

    validating store structure

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todos';
+import { Todo } from '../actions';
 
-export const rootReducer = combineReducers({
-  counter: () => 1
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const rootReducer = combineReducers<StoreState>({
+  todos: todosReducer
 });
